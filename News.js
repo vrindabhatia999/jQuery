f09e2df7ef9ba4e0f4bacd7d1b7e9ffee416feee
Vrinda Bhatia <vrindabhatia999@gmail.com>
2020-11-19 16:32:36 +0530

    commit

---
+++ News.js
@@ -0,0 +1,3 @@
+var a='Covid Updates'
+var link1=a.link("https://www.google.com/search?q=corona+cases+in+delhi&rlz=1C1CHBF_enIN814IN814&oq=coro&aqs=chrome.1.69i57j35i39l2j0i67i395i433j0i67i131i395i433l3j69i60.2360j1j7&sourceid=chrome&ie=UTF-8")
+panel.innerHTML=link1;
